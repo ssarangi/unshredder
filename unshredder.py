a613b5354078e03ed94054be709840afa876aae4
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2016-03-06 17:30:20 -0800

    Added unshredder initial code

---
+++ unshredder.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import sys
+
+def read_img(filename):
+    im = Image.open(filename)
+    return im
+
+def main():
+    filename = sys.argv[1]
+    
+if __name__ == "__main__":
+    main()
